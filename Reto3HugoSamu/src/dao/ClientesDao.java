5ec2984d7cd7c428552f6504011e7e92d5f2449a
Samuel <Samuel@PC-HP-SAMUEL>
2025-05-12 13:47:42 +0200

    metodo inserta en daoClientes

--- Reto3HugoSamu/src/dao/ClientesDao.java
+++ Reto3HugoSamu/src/dao/ClientesDao.java
@@ -3,6 +3,7 @@ package dao;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -28,5 +29,24 @@ public class ClientesDao {
 			Conexion.cierraConexion();
 		}
 		return lista;
+	}
+	public static void inserta(Clientes clien) {
+		try {
+			Connection con= Conexion.abreConexion();
+			PreparedStatement pst=con.prepareStatement("insert into clientes(nombre,direccion,codigo) values (?,?,?)", Statement.RETURN_GENERATED_KEYS);
+			pst.setString(1, clien.getNombre());
+			pst.setString(2, clien.getDireccion());
+			pst.setInt(3, clien.getCodigo());
+			pst.executeUpdate();
+			ResultSet rs=pst.getGeneratedKeys();
+			if (rs.next()) {
+			clien.setIdCliente(rs.getInt(1));
+			}rs.close();
+		
+		} catch (Exception e) {
+			e.printStackTrace();
+		}finally {
+			Conexion.cierraConexion();
+		}
 	}
 }
