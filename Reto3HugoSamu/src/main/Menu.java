df799f0c49079df1bf3852d389d8d64b92a2d573
Samuel <Samuel@PC-HP-SAMUEL>
2025-05-15 09:08:09 +0200

    menu opcion 1(mantenimientos completada)

--- Reto3HugoSamu/src/main/Menu.java
+++ Reto3HugoSamu/src/main/Menu.java
@@ -1,9 +1,11 @@
 package main;
 
+import java.util.List;
 import java.util.Scanner;
 
 import clases.Categorias;
 import clases.Clientes;
+import clases.Productos;
 
 public class Menu {
 
@@ -41,7 +43,7 @@ public class Menu {
 		int opcion2 = 0;
 		do {
 			opcion2 = util.Validarfunciones.dimeEntero(
-					"1.1-.Gestion de categorias \n1.2-.Gestion de productos\n3-.Gestion de clientes\n4-.Salir\nIntroduce una opcion",
+					"1.1-.Gestion de categorias \n1.2-.Gestion de productos\n1.3-.Gestion de clientes\n4-.Salir\nIntroduce una opcion",
 					sc);
 			switch (opcion2) {
 			case 1:
@@ -52,6 +54,23 @@ public class Menu {
 				break;
 			case 2:
 
+				String nombreProducto = util.Validarfunciones.dimeString("Introduce un nombre para el producto", sc);
+				double precioProducto = util.Validarfunciones.dimeDouble("Introduce el precio del producto", sc);
+				String descripcion = util.Validarfunciones.dimeString("Introduce una descripcion para el producto", sc);
+				String colorProducto = util.Validarfunciones.dimeString("Introduce el color del producto", sc);
+				String tallaProducto = util.Validarfunciones.dimeString("Introduce la talla del producto", sc);
+				int stock = util.Validarfunciones.dimeEntero("Introduce la cantidad de stock", sc);
+
+				mostrarCategorias();
+
+				int categoriaElegida = util.Validarfunciones.dimeEntero("Introduce una opcion", sc);
+				/// verifico que existe la categoria y la devuelvo para insertar el produto en
+				/// ella
+
+				Categorias c = verificarCategoria(categoriaElegida);
+				Productos p = new Productos(1, c, nombreProducto, precioProducto, descripcion, colorProducto,
+						tallaProducto, stock);
+				dao.ProductosDao.insertaProducto(p);
 				break;
 
 			case 3:
@@ -62,6 +81,26 @@ public class Menu {
 				break;
 			}
 		} while (opcion2 > 3 || opcion2 <= 0);
+	}
+
+	public static Categorias verificarCategoria(int categoriaElegida) {
+		Categorias catElegida = null;
+		List<Categorias> lista = dao.CategoriasDao.lista();
+		for (Categorias categorias : lista) {
+			if (categorias.getIdCategoria() == categoriaElegida) {
+				catElegida = categorias;
+
+			}
+
+		}
+		return catElegida;
+	}
+
+	public static void mostrarCategorias() {
+		List<Categorias> lista = dao.CategoriasDao.lista();
+		for (Categorias categorias : lista) {
+			System.out.println(categorias);
+		}
 	}
 
 	public static void menuGestionClientes() {
@@ -79,21 +118,25 @@ public class Menu {
 				dao.ClientesDao.inserta(c);
 				break;
 			case 2:
-				int codBusqueda=util.Validarfunciones.dimeEntero("Introduce el codigo por el que buscar al cliente", sc);
-				Clientes cl= dao.ClientesDao.buscarXcodigo(codBusqueda);
-			if (cl==null) {
-				System.out.println("El cliente no existe");
-				
-			}else {
-				System.out.println(cl);
-				String nombreCliente2 = util.Validarfunciones.dimeString("Introduce el nombre para actualizar al cliente", sc);
-				String direccionCliente2 = util.Validarfunciones.dimeString("Introduce la nueva direccion para actualizar al cliente", sc);
-				int codigoCliente2 = util.Validarfunciones.dimeEntero("Introduce el codigo para actualizar al cliente", sc);
-				Clientes c2 = new Clientes(cl.getIdCliente(), nombreCliente2, direccionCliente2, codigoCliente2);
-				dao.ClientesDao.actualiza(c2);
-				System.out.println(c2);
-			}
-			
+				int codBusqueda = util.Validarfunciones.dimeEntero("Introduce el codigo por el que buscar al cliente",
+						sc);
+				Clientes cl = dao.ClientesDao.buscarXcodigo(codBusqueda);
+				System.out.println(dao.ClientesDao.buscarXcodigo(codBusqueda));
+				if (cl == null) {
+					System.out.println("El cliente no existe");
+
+				} else {
+					System.out.println(cl);
+					String nombreCliente2 = util.Validarfunciones
+							.dimeString("Introduce el nombre para actualizar al cliente", sc);
+					String direccionCliente2 = util.Validarfunciones
+							.dimeString("Introduce la nueva direccion para actualizar al cliente", sc);
+					int codigoCliente2 = util.Validarfunciones
+							.dimeEntero("Introduce el codigo para actualizar al cliente", sc);
+					Clientes c2 = new Clientes(cl.getIdCliente(), nombreCliente2, direccionCliente2, codigoCliente2);
+					dao.ClientesDao.actualiza(c2);
+					System.out.println(c2);
+				}
 
 				break;
 			default:
